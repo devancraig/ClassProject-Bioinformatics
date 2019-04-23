43538d8ffbdca96a628ce0b69870ee5c2d6c50d2
Devan Craig <devancraig@u.boisestate.edu>
2019-04-23 07:43:31 -0600

    Declared some varibles for the BTree class

--- eclipse-workspace/ClassProject-Bioinformatics/src/BTree.java
+++ eclipse-workspace/ClassProject-Bioinformatics/src/BTree.java
@@ -1,9 +1,22 @@
+import java.io.File;
 
 public class BTree {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int degree, numNodes, suqLength;
+	private BTreeNode current, root, next;
+	private File file;
+	
+	public BTree(int degree, int length, File file) {
+		
+		this.degree = degree;
+		suqLength = length;
+		this.file = file;
+		
+		root = new BTreeNode(this.degree, true, true, 0);
+		numNodes =1;
 	}
+	
+	
+	
+	
 
 }
