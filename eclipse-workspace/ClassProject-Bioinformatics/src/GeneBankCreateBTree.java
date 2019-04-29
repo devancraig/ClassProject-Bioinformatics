ba6ec912e0eefd3db0fc466800df77d011e2c975
jakeandrews186 <jakeandrews186@u.boisestate.edu>
2019-04-29 12:59:12 -0600

    Started creation of the GeneBankCreateBTree file.

--- eclipse-workspace/ClassProject-Bioinformatics/src/GeneBankCreateBTree.java
+++ eclipse-workspace/ClassProject-Bioinformatics/src/GeneBankCreateBTree.java
@@ -4,10 +4,45 @@
  *
  */
 public class GeneBankCreateBTree {
+	static int cacheYN, degree, seqLength;
+	static String file;
 
+	
+	/**
+	 * Main method for creating the BTree from the user's specifications. 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+		cacheYN = Integer.parseInt(args[1]);
+			if (!(cacheYN == 0 || cacheYN == 1))
+			{
+				Usage();
+			}
+		degree = Integer.parseInt(args[2]);
+		file = args[3];
+		seqLength = Integer.parseInt(args[4]);
+			if (seqLength > 31)
+			{
+				System.err.println("Maximum Sequence Size is 31");
+				Usage();
+			}
+		} catch (Exception e)
+		{
+			Usage();
+		}
+		Parser parse = new Parser(file, seqLength);
 
+		
+	}
+	
+	/**
+	 * Errors out with the correct usage of the java file. 
+	 */
+	public static void Usage()
+	{
+		System.err.println("Usage: java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence" + 
+			"length> [<cache size>] [<debug level>]");
 	}
 
 }
